081ab64b3008c0084bcbff9559c365f94e62e7c6
Renohun <127425333+Renohun@users.noreply.github.com>
2024-03-29 18:00:43 +0100

    Final update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # school-calculator
  
+Well this a simple web calculator, nothing special.
+Altough sadly we have make the calculator the way you it's being made.
+Which makes my life harder for no reason
--- kalkulalas.js
+++ kalkulalas.js
@@ -1,85 +1,114 @@
 function num_one(){
-    let kijelzo = document.getElementById('display').innerHTML += "1"
-    console.log("1"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "1"
+    }  
 }
 
 function num_two(){
-    let kijelzo = document.getElementById('display').innerHTML += "2"
-    console.log("2"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "2"
+        }
 }
 
 function num_three(){
-    let kijelzo = document.getElementById('display').innerHTML += "3"
-    console.log("3"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "3"
+        }
 }
 
 function num_four(){
-    let kijelzo = document.getElementById('display').innerHTML += "4"
-    console.log("4"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "4"
+        }
 }
 
 function num_five(){
-    let kijelzo = document.getElementById('display').innerHTML += "5"
-    console.log("5"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "5"
+        }
 }
 
 function num_six(){
-    let kijelzo = document.getElementById('display').innerHTML += "6"
-    console.log("6"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "6"
+        }
 }
 
 function num_seven(){
-    let kijelzo = document.getElementById('display').innerHTML += "7"
-    console.log("7"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "7"
+       }
 }
 
 function num_eight(){
-    let kijelzo = document.getElementById('display').innerHTML += "8"
-    console.log("8"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "8"
+        }
 }
 
 function num_nine(){
-    let kijelzo = document.getElementById('display').innerHTML += "9"
-    console.log("9"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "9"
+       }
+}
+
+function num_zero(){
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "0"
+       }
 }
 
 
 function char_com (){
-    let kijelzo = document.getElementById('display').innerHTML += ","
-    console.log(","); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "."
+        }
 }
 
 function op_div(){
-    let kijelzo = document.getElementById('display').innerHTML += "/"
-    console.log("/"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "/"
+        }
 }
 
 function op_mul(){
-    let kijelzo = document.getElementById('display').innerHTML += "x"
-    console.log("*"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "*"
+       }
 }
 
 function op_add(){
-    let kijelzo = document.getElementById('display').innerHTML += "+"
-    console.log("+"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "+"
+        }
 }
 
 function op_minus(){
-    let kijelzo = document.getElementById('display').innerHTML += "-"
-    console.log("-"); console.log("   "); console.log(kijelzo);
+    if(kijelzo.length < 34){
+        kijelzo = document.getElementById('display').innerHTML += "-"
+        }
 }
 
 function C_button(){
-    let kijelzo = document.getElementById('display').innerHTML = ""
+    kijelzo = document.getElementById('display').innerHTML = ""
 }
 
 function Torles(){
-    console.log(typeof kijelzo)
+    kijelzo = document.getElementById('display').innerHTML = kijelzo.slice(0, -1)
 }
 
-function szamitas (kijelzo) {
+function szamitas (){
+    try{
+        let result = eval(kijelzo)
+        document.getElementById('display').innerHTML = result
+    }
+    catch(error){
+        document.getElementById('display').innerHTML = "Error"
+    }
 
 }
+
+let kijelzo = ""
 
 let one = document.getElementById('egy')
 let two = document.getElementById('ketto')
@@ -90,7 +119,7 @@ let six = document.getElementById('hat')
 let seven = document.getElementById('het')
 let eight = document.getElementById('nyolc')
 let nine = document.getElementById('kilenc')
-let zero = document.getElementById('nulll')
+let zero = document.getElementById('nulla')
 
 let comma = document.getElementById('vesszo')
 let divide = document.getElementById('osztas')
@@ -120,6 +149,7 @@ six.addEventListener("click", num_six)
 seven.addEventListener("click", num_seven)
 eight.addEventListener("click", num_eight)
 nine.addEventListener("click", num_nine)
+zero.addEventListener("click", num_zero)
 
 
 equal.addEventListener("click", szamitas)
@@ -129,3 +159,4 @@ divide.addEventListener("click", op_div)
 multiply.addEventListener("click", op_mul)
 add.addEventListener("click", op_add)
 minus.addEventListener("click", op_minus)
+
